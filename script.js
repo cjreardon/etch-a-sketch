3e64b2e7fec4f9cd59a769404518890a0c1e1aef
cjreardon <connorreardon3@gmail.com>
2022-09-26 16:02:27 -0400

    Add blue background trace for hovering

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 create();
 
-function create(){
-    for(var i=0; i<16*16; i++){
-        makeGrid();
-    }
+document.querySelectorAll('.square').forEach(item => {
+    item.addEventListener('mouseover', event => {
+      item.classList.add('hover');
+    })
+  })
+
+function create() {
+  for (var i = 0; i < 16 * 16; i++) {
+    makeGrid();
+  }
 }
 
-function makeGrid(){
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode(" ");
-    newDiv.appendChild(newContent);
-    newDiv.classList.add("square");
-    const currentDiv = document.getElementById("gridContainer");
-    currentDiv.appendChild(newDiv);
+function makeGrid() {
+  const newDiv = document.createElement("div");
+  const newContent = document.createTextNode(" ");
+  newDiv.appendChild(newContent);
+  newDiv.classList.add("square");
+  const currentDiv = document.getElementById("gridContainer");
+  currentDiv.appendChild(newDiv);
 }
