ebe6a048f2a5fd7f6dcc2db510350a8fc371434d
cjreardon <connorreardon3@gmail.com>
2022-09-27 19:27:58 -0400

    Add the ability to resize the grid

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 create();
 
-document.querySelectorAll('.square').forEach(item => {
-    item.addEventListener('mouseover', event => {
-      item.classList.add('hover');
-    })
-  })
+function etch() {
+  document.querySelectorAll(".square").forEach((item) => {
+    item.addEventListener("mouseover", (event) => {
+      item.classList.add("hover");
+    });
+  });
+}
+
+function getDimensions() {
+  var dims = prompt("Please enter new squares per side (less than 100)");
+  while (Number(dims) > 100) {
+    dims = prompt("Please enter new squares per side (less than 100)");
+  }
+  updateGrid(dims);
+  return Number(dims);
+}
+
+function updateGrid(newDim) {
+  let parent = document.getElementById("gridContainer");
+  let child = parent.lastElementChild;
+  while (child) {
+    parent.removeChild(child);
+    child = parent.lastElementChild;
+  }
+  for (var i = 0; i < newDim * newDim; i++) {
+    makeGrid();
+  }
+  newPadding = (704 - (newDim * 2)) / newDim / 2;
+  console.log(newPadding);
+  document.querySelectorAll(".square").forEach((item) => {
+     item.style.padding = newPadding + "px";
+    });
+  etch();
+}
 
 function create() {
   for (var i = 0; i < 16 * 16; i++) {
     makeGrid();
   }
+  etch();
 }
 
 function makeGrid() {
